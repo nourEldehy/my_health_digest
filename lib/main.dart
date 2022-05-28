190607e41445231f6bac9d4d22b68e4ff7c655b9
nourEldehy <nour_dehy@yahoo.com>
2022-05-28 18:41:47 +0200

    Symptoms Checker is Integrated Now with the whole app. The clinic is now redirecting to the clinic website.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:training_and_diet_app/ui/pages/profile_screen.dart';
-// import 'package:training_and_diet_app/ui/pages/symptoms.dart';
 
 
 void main() => runApp(MyApp());
